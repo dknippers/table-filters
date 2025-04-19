0979ae7c94aa9ff84da5ddc5dca416cd4c38c220
Daniël Knippers <dknippers@gmail.com>
2025-04-19 10:37:44 +0200

    Add PeopleComponent

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,9 @@
 <script setup lang="ts">
-import TableComponent from "./components/table/TableComponent.vue";
-import type { Column } from "./components/table/types";
-import Text from "./components/Text.vue";
-
-type Person = {
-  name: string;
-  age: number;
-};
-
-const p1: Person = {
-  name: "dani",
-  age: 37,
-}
-
-const p2: Person = {
-  name: "chen",
-  age: 38
-}
-
-const columns: Column<Person>[] = [
-  {
-    header: "Name",
-    value: person => person.name,
-  },
-  {
-    header: "Age",
-    render: person => ({
-      component: Text,
-      props: {
-        text: person.age
-      }
-    })
-  },
-  {
-    header: "Combined",
-    value: person => `${person.name} is ${person.age} years old`,
-  },
-]
-const data = [p1, p2];
-
+import PeopleComponent from "./components/people/PeopleComponent.vue";
 </script>
 
 <template>
   <main>
-    <TableComponent :columns="columns" :data="data" />
+    <PeopleComponent />
   </main>
 </template>
---
+++ src/components/people/PeopleComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import TableComponent from '../table/TableComponent.vue';
+import type { Column } from '../table/types';
+import TextComponent from '../TextComponent.vue';
+import type { Person } from './types';
+
+const p1: Person = {
+    name: "dani",
+    age: 37,
+}
+
+const p2: Person = {
+    name: "chen",
+    age: 38
+}
+
+const columns: Column<Person>[] = [
+    {
+        header: "Name",
+        value: person => person.name,
+    },
+    {
+        header: "Age",
+        render: person => ({
+            component: TextComponent,
+            props: {
+                text: `${person.age}`
+            }
+        })
+    },
+    {
+        header: "Combined",
+        value: person => `${person.name} is ${person.age} years old`,
+    },
+]
+const data = [p1, p2];
+
+</script>
+
+<template>
+    <TableComponent :columns="columns" :data="data" />
+</template>
