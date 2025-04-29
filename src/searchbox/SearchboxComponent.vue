6678c8fac151cc67efeef46ecbc2657c6ea595a5
Daniël Knippers <dknippers@gmail.com>
2025-04-29 22:25:35 +0200

    Add debounce to Searchbox

--- src/searchbox/SearchboxComponent.vue
+++ src/searchbox/SearchboxComponent.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { debounce } from '@/utils/utils';
+import { computed } from 'vue';
+
 const model = defineModel<string>({ required: true });
 defineProps<{ placeholder?: string }>();
+
+const debouncedSetValue = debounce((v: string) => (model.value = v), 400);
+
+const input = computed({
+  get: () => model.value,
+  set: debouncedSetValue,
+});
 </script>
 
 <template>
-  <input type="text" v-model="model" :placeholder="placeholder" />
+  <input type="text" v-model="input" :placeholder="placeholder" />
 </template>
 
 <style scoped>
